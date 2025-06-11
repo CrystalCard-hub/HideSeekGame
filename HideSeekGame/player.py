073f57164628b0f21a7e5df833a125c067e59a9f
CrystalCard-hub <153093579+CrystalCard-hub@users.noreply.github.com>
2025-06-11 08:57:15 +0300

    player.py

---
+++ HideSeekGame/player.py
@@ -0,0 +1,21 @@
+import pygame
+
+class Player:
+    def __init__(self, x, y):
+        self.image = pygame.Surface((40, 40))
+        self.image.fill((0, 0, 255))  # Blue color for player
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.speed = 5
+
+    def move(self, keys):
+        if keys[pygame.K_w]:
+            self.rect.y -= self.speed
+        if keys[pygame.K_s]:
+            self.rect.y += self.speed
+        if keys[pygame.K_a]:
+            self.rect.x -= self.speed
+        if keys[pygame.K_d]:
+            self.rect.x += self.speed
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
